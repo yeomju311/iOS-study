52d469b17cd603bee8e2025d4303170103b05595
최윤주 <yeomju311@gmail.com>
2019-12-09 23:56:45 +0900

    make project

---
+++ Poster/Poster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poster
+//
+//  Created by 최윤주 on 09/12/2019.
+//  Copyright © 2019 yunjjuice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
