846a7dfe9fd456da222d002658cfebda43ac006d
최윤주 <yeomju311@gmail.com>
2020-03-22 17:56:40 +0900

    나의 첫앱 도전 - 끝

--- Poster/Poster/ViewController.swift
+++ Poster/Poster/ViewController.swift
@@ -10,29 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var currentValue:Int = 0;
+	@IBOutlet weak var bountyLabel: UILabel!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		refresh()
 	}
 
 	@IBAction func showAlert() {
-		//print("hello world")
-		
 		// add alert
-		let alert = UIAlertController(title: "hello", message: "first app", preferredStyle: .alert)
-		let action = UIAlertAction(title: "ok", style: .default, handler: nil)
+		let message = "현상금은 \(currentValue) 입니다"
+		let alert = UIAlertController(title: "hello", message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: "ok", style: .default, handler: { action in
+			self.refresh()
+		}) // closer
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
 	}
 	
-	@IBAction func challenge() {
-		// add alert
-		let alert = UIAlertController(title: "hello", message: "도전", preferredStyle: .alert)
-		let action = UIAlertAction(title: "ok", style: .default, handler: nil)
-		alert.addAction(action)
-		present(alert, animated: true, completion: nil)
+	func refresh() {
+		let randomNum = arc4random_uniform(100000) + 1
+		currentValue = Int(randomNum)
+		bountyLabel.text = "$ \(currentValue)"
 	}
-	
-
 }
 
